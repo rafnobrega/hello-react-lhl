572b856f6eb767688664e38ec2a1e8c418e96c8f
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-06-13 12:14:22 -0700

    add logic to index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 const Button = (props) => {
-  // your code here
+  const { resetValues } = props;
+  console.log("props:::", props);
+
+  return <button onClick={resetValues}>{props.children}</button>;
 };
 
 const Application = () => {
-
-  // your code here
+  const [name, setName] = useState("");
 
   const reset = () => {
     console.log("reset");
-    // your code here
+    setName("");
   };
 
   return (
     <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input 
+      placeholder="Type your name" 
+      value={name}
+      onChange={(event) => setName(event.target.value)}
+      >
+      </input>
+      <Button resetValues={reset}>Reset</Button>
+      {/* {name && <h1>Hello {name}</h1>}
+      {!name && <h1></h1>} */}
+      {name ? <h1>Hello {name}</h1> : <h1></h1>}
     </main>
   );
 };
